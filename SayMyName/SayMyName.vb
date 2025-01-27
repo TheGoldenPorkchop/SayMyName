a011f1044b7e27cf882ac69949a85d7b1572f674
TheGoldenPorkchop <angelnavamusic@gmail.com>
2025-01-26 20:26:37 -0700

    Add project files.

---
+++ SayMyName/SayMyName.vb
@@ -0,0 +1,21 @@
+﻿Module SayMyName
+
+    Sub Main()
+        Dim userName As String
+
+        Console.WriteLine("Give me your name: ")
+        userName = Console.ReadLine()
+
+        If userName = "Tim" Then
+            Console.WriteLine("You should give me extra credit for remembering your name")
+        ElseIf userName = "Angel" Then
+            Console.WriteLine("Woah, that's the programmer")
+        ElseIf userName = "1" Then
+            Console.WriteLine("haha, 1")
+        Else
+            Console.WriteLine(vbNewLine & "Hello, " & userName)
+        End If
+
+    End Sub
+
+End Module
